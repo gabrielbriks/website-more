d1c19536aa680094a3606343af99ea85091d81eb
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2024-02-14 20:28:17 -0300

    Run lint fix

--- src/app/[lang]/(app)/who-we-are/page.tsx
+++ src/app/[lang]/(app)/who-we-are/page.tsx
@@ -1,13 +1,13 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-	title: "Who We Are",
-	description: "",
+	title: 'Who We Are',
+	description: '',
 };
 
 export default function WhoWeAre() {
 	return (
-		<div className="font-kanit flex h-full w-full flex-1 flex-col">
+		<div className="flex h-full w-full flex-1 flex-col font-kanit">
 			<h1 className="text-center text-3xl">Who We Are</h1>
 		</div>
 	);
--- src/i18n-config.ts
+++ src/i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
-  defaultLocale: "en",
-  locales: ["en", "ja", "pt"],
+	defaultLocale: 'en',
+	locales: ['en', 'ja', 'pt'],
 } as const;
 
-export type Locale = (typeof i18n)["locales"][number];
+export type Locale = (typeof i18n)['locales'][number];
