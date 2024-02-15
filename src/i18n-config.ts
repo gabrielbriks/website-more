07d48703b90cecff31288883c3eb978255555965
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2024-02-15 14:08:24 -0300

    Adiciona lembrete de refinamento no `i18n-config.ts`

--- src/i18n-config.ts
+++ src/i18n-config.ts
@@ -1,6 +1,8 @@
 export const i18n = {
-	defaultLocale: 'en',
+	defaultLocale: 'en', //TODO: Alterar para 'ja'
 	locales: ['en', 'ja', 'pt'],
 } as const;
+
+//TODO: Rever aspectos do video(https://www.youtube.com/watch?v=1S7yFzU2ulg) para refinamento
 
 export type Locale = (typeof i18n)['locales'][number];
