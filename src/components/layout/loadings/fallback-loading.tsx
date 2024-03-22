daf1c43ed164c89835f9d535c0e4d67298a0ca77
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2024-03-22 13:54:39 -0300

    Implementa novo loading e adiciona melhorias no loading-global

--- src/components/layout/loadings/fallback-loading.tsx
+++ src/components/layout/loadings/fallback-loading.tsx
@@ -1,10 +1,86 @@
-//TODO: move for new component
+'use client';
+import ClickMoreLogoWhite from '@/assets/click-more-purple.svg';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+const container = {
+	hidden: { opacity: 1, scale: 0 },
+	visible: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			delayChildren: 0.3,
+			staggerChildren: 0.2,
+		},
+	},
+};
+
 const FallbackLoading = () => (
-	<div className="flex min-h-screen min-w-full flex-1 items-center justify-center ">
-		<span className="relative flex h-3 w-3">
-			<span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-purple-500 opacity-75"></span>
-			<span className="relative inline-flex h-3 w-3 rounded-full bg-purple-700"></span>
-		</span>
+	<div className="flex min-h-screen min-w-full flex-col items-center justify-center gap-5 bg-zinc-100">
+		<motion.div
+			initial={{ scale: 0 }}
+			animate={{ scale: 1 }}
+			transition={{
+				type: 'spring',
+				stiffness: 50,
+				damping: 10,
+			}}
+			className="flex w-full items-center justify-center"
+		>
+			<Image
+				alt="Logo click more purple "
+				loading="lazy"
+				src={ClickMoreLogoWhite}
+				className=" aspect-[4.76] w-[250px] max-w-full"
+			/>
+		</motion.div>
+
+		<div className="flex min-h-full min-w-full items-center justify-center gap-5 bg-zinc-100">
+			<motion.span
+				initial={{ scale: 0 }}
+				animate={{ scale: 1 }}
+				transition={{
+					type: 'spring',
+					stiffness: 50,
+					damping: 10,
+					duration: 0.8,
+				}}
+				className="relative flex h-5 w-5"
+			>
+				<span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-purple-500 opacity-75"></span>
+				<span className="relative inline-flex h-5 w-5 rounded-full bg-purple-700"></span>
+			</motion.span>
+
+			<motion.span
+				initial={{ scale: 0 }}
+				animate={{ scale: 1 }}
+				transition={{
+					type: 'spring',
+					stiffness: 50,
+					damping: 10,
+					duration: 1,
+				}}
+				className="relative flex h-5 w-5"
+			>
+				<span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-purple-500 opacity-75"></span>
+				<span className="relative inline-flex h-5 w-5 rounded-full bg-purple-700"></span>
+			</motion.span>
+
+			<motion.span
+				initial={{ scale: 0 }}
+				animate={{ scale: 1 }}
+				transition={{
+					type: 'spring',
+					stiffness: 50,
+					damping: 10,
+					duration: 2,
+				}}
+				className="relative flex h-5 w-5"
+			>
+				<span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-purple-500 opacity-75"></span>
+				<span className="relative inline-flex h-5 w-5 rounded-full bg-purple-700"></span>
+			</motion.span>
+		</div>
 	</div>
 );
 
--- src/components/layout/loadings/loading-global.tsx
+++ src/components/layout/loadings/loading-global.tsx
@@ -1,10 +1,21 @@
+import ClickMoreLogoWhite from '@/assets/clickmore-logo-white.svg';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 
 export default function LoaderGlobal() {
 	return (
-		<div className="absolute z-50 flex max-h-full min-h-screen w-full items-center justify-center bg-cm-gradient-imgcolor">
+		<div className="absolute z-50 flex max-h-full min-h-screen w-full flex-col items-center justify-center bg-cm-gradient-imgcolor">
+			<div>
+				<Image
+					alt="Logo click more purple "
+					loading="eager"
+					src={ClickMoreLogoWhite}
+					priority={true}
+					className=" aspect-[4.76] w-[250px] max-w-full"
+				/>
+			</div>
 			<div className="relative rounded-md p-4">
-				<div className="flex justify-center">
+				<div className="flex justify-center gap-5">
 					<>
 						<motion.span
 							className="mx-1 my-12 h-4 w-4 rounded-full bg-white"
