9aa9a2b01782415541aef9eb73591042bf979bf4
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2024-04-10 20:08:42 -0300

    Ajustes em propriedades svgs

--- src/components/header/menu-mobile-drawer.tsx
+++ src/components/header/menu-mobile-drawer.tsx
@@ -59,9 +59,9 @@ export default function MenuMobileDrawer({ lang }: MenuMobileDrawerProps) {
 					>
 						<path
 							stroke="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
 							d="M1 1h15M1 7h15M1 13h15"
 						/>
 					</svg>
@@ -127,9 +127,9 @@ export default function MenuMobileDrawer({ lang }: MenuMobileDrawerProps) {
 	// 				>
 	// 					<path
 	// 						stroke="currentColor"
-	// 						stroke-linecap="round"
-	// 						stroke-linejoin="round"
-	// 						stroke-width="2"
+	// 						strokeLinecap="round"
+	// 						strokeLinejoin="round"
+	// 						strokeWidth="2"
 	// 						d="M1 1h15M1 7h15M1 13h15"
 	// 					/>
 	// 				</svg>
